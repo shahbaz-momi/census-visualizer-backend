a7e140b4903a01c3f73b0039472e3e76100caa1b
Shahbaz Momi <shahbaz.man3@gmail.com>
2020-08-06 22:47:09 -0400

    Fixed using summations

--- src/main/kotlin/com/beanz/censusviz/repos/DatasetEducationRepo.kt
+++ src/main/kotlin/com/beanz/censusviz/repos/DatasetEducationRepo.kt
@@ -10,7 +10,7 @@ import javax.persistence.Table
 @Table(name = "education")
 interface DatasetEducationRepo : CrudRepository<DDatasetRecord, Int> {
 
-    @Query("SELECT e.value / p.value as count, g.lat, g.lon FROM education e NATURAL JOIN geocode_lut g, population p WHERE e.meta IN :metas AND e.age IN :ages AND e.sex = :sex AND e.geocode = g.geocode AND p.geocode = g.geocode GROUP BY g.geocode, g.lon, g.lat", nativeQuery = true)
+    @Query("SELECT SUM(e.value) / SUM(p.value) as count, g.lat, g.lon FROM education e NATURAL JOIN geocode_lut g, population p WHERE e.meta IN :metas AND e.age IN :ages AND e.sex = :sex AND e.geocode = g.geocode AND p.geocode = g.geocode GROUP BY g.geocode, g.lon, g.lat", nativeQuery = true)
     fun findAllByAgeInAndSexAndMetaIn(@Param("ages") ages: List<Int>, @Param("sex") sex: Int, @Param("metas") metas: List<Int>): List<DDatasetDoubleCombinedRecord>
 
 }
