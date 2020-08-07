90038a9c0537cfb056b9b38968a85cc396ffe464
Shahbaz Momi <shahbaz.man3@gmail.com>
2020-08-06 23:27:30 -0400

    Added web build

---
+++ src/main/resources/static/precache-manifest.385079d4511c16501a65189b0abb6c6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fe4615bfb58cda1de53251580897b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "231511abf0d97befe4cb",
+    "url": "/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "faebd23c7b030735a492",
+    "url": "/static/css/main.05f392c1.chunk.css"
+  },
+  {
+    "revision": "231511abf0d97befe4cb",
+    "url": "/static/js/2.e2ad9a7b.chunk.js"
+  },
+  {
+    "revision": "4c768f233948a0824af9e0a52e2348c1",
+    "url": "/static/js/2.e2ad9a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faebd23c7b030735a492",
+    "url": "/static/js/main.e8ce7a92.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
